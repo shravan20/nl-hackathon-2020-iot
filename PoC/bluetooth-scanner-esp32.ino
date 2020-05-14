d2364548a11ef3f3c94ba500c110034f0bea44d6
shravankb <shravan.kumar@nineleaps.com>
2020-05-14 20:30:22 +0530

    Interfacing Uno with ESP32

---
+++ PoC/bluetooth-scanner-esp32.ino
@@ -0,0 +1,54 @@
+/*
+
+	Simple Code to scan the Bluetooth devices around you and print the details with count
+	
+	Trying to interface ESP32 with Arduino Uno 
+   
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial sw( 2, 3 ); //RX, TX
+
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
+
+
+void bluetoothScanner() {
+
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(2000);
+   
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Scanning...");
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+  sw.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  bluetoothScanner();
+}
